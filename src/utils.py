84c04855fd19c8d84f592cdbee6bf1bf225528eb
Ziqi Zhang <ziqizhang@ipsec-172-16-85-74.vpn.gatech.edu>
2021-03-01 15:30:56 -0500

    preprocessed

---
+++ src/utils.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+from scipy import stats
+
+def quantile_norm(X):
+    """Normalize the columns of X to each have the same distribution.
+
+    Given an expression matrix (microarray data, read counts, etc) of M genes
+    by N samples, quantile normalization ensures all samples have the same
+    spread of data (by construction).
+
+    The data across each row are averaged to obtain an average column. Each
+    column quantile is replaced with the corresponding quantile of the average
+    column.
+
+    Parameters
+    ----------
+    X : 2D array of float, shape (M, N)
+        The input data, with M rows (genes/features) and N columns (samples).
+
+    Returns
+    -------
+    Xn : 2D array of float, shape (M, N)
+        The normalized data.
+    """
+    # compute the quantiles
+    quantiles = np.mean(np.sort(X, axis=0), axis=1)
+
+    # compute the column-wise ranks. Each observation is replaced with its
+    # rank in that column: the smallest observation is replaced by 1, the
+    # second-smallest by 2, ..., and the largest by M, the number of rows.
+    ranks = np.apply_along_axis(stats.rankdata, 0, X)
+
+    # convert ranks to integer indices from 0 to M-1
+    rank_indices = ranks.astype(int) - 1
+
+    # index the quantiles for each rank with the ranks matrix
+    Xn = quantiles[rank_indices]
+
+    return(Xn)
+
+def quantile_norm_log(X):
+    logX = np.log1p(X)
+    logXn = quantile_norm(logX)
+    return logXn
+
+def preprocess(counts, mode = "standard", modality = "RNA"):
+    """\
+    Description:
+    ------------
+    Preprocess the dataset
+    """
+    if mode == "standard":
+        if modality == "ATAC":
+            counts = (counts > 0).astype(np.float)
+            
+        # normalize according to the library size
+        libsize = np.median(np.sum(counts, axis = 1))
+        counts = counts / np.sum(counts, axis = 1)[:,None] * libsize
+
+        if modality == "RNA":
+            counts = np.log1p(counts)
+
+    elif mode == "quantile":
+        if modality == "RNA":
+            counts = quantile_norm_log(counts)
+        else:
+            # make binary
+            counts = (counts > 0).astype(np.float) 
+    return counts
+
+def plot_latent(z1, z2, anno1 = None, anno2 = None, mode = "joint", save = None, figsize = (20,10), axis_label = "Latent", **kwargs):
+    """\
+    Description
+        Plot latent space
+    Parameters
+        z1
+            the latent space of first data batch, of the shape (n_samples, n_dimensions)
+        z2
+            the latent space of the second data batch, of the shape (n_samples, n_dimensions)
+        anno1
+            the cluster annotation of the first data batch, of the  shape (n_samples,)
+        anno2
+            the cluster annotation of the second data batch, of the  shape (n_samples,)
+        mode
+            "joint": plot two latent spaces(from two batches) into one figure
+            "separate" plot two latent spaces separately
+        save
+            file name for the figure
+        figsize
+            figure size
+    """
+    _kwargs = {
+        "s": 10,
+        "alpha": 0.9,
+    }
+    _kwargs.update(kwargs)
+
+    fig = plt.figure(figsize = figsize)
+    if mode == "modality":
+        colormap = plt.cm.get_cmap("tab10")
+        ax = fig.add_subplot()
+        ax.scatter(z1[:,0], z1[:,1], color = colormap(1), label = "scRNA-Seq", **_kwargs)
+        ax.scatter(z2[:,0], z2[:,1], color = colormap(2), label = "scATAC-Seq", **_kwargs)
+        ax.legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = 1, bbox_to_anchor=(1.04, 1))
+        
+        ax.tick_params(axis = "both", which = "major", labelsize = 15)
+
+        ax.set_xlabel(axis_label + " 1", fontsize = 19)
+        ax.set_ylabel(axis_label + " 2", fontsize = 19)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)  
+
+    elif mode == "joint":
+        ax = fig.add_subplot()
+        cluster_types = set([x for x in np.unique(anno1)]).union(set([x for x in np.unique(anno2)]))
+        colormap = plt.cm.get_cmap("tab20", len(cluster_types))
+
+        for i, cluster_type in enumerate(cluster_types):
+            index = np.where(anno1 == cluster_type)[0]
+            index2 = np.where(anno2 == cluster_type)[0]
+            ax.scatter(np.concatenate((z1[index,0], z2[index2,0])), np.concatenate((z1[index,1],z2[index2,1])), color = colormap(i), label = cluster_type, **_kwargs)
+        
+        ax.legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = 1, bbox_to_anchor=(1.04, 1))
+        
+        ax.tick_params(axis = "both", which = "major", labelsize = 15)
+
+        ax.set_xlabel(axis_label + " 1", fontsize = 19)
+        ax.set_ylabel(axis_label + " 2", fontsize = 19)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)  
+
+
+    elif mode == "separate":
+        axs = fig.subplots(1,2)
+        cluster_types = set([x for x in np.unique(anno1)]).union(set([x for x in np.unique(anno2)]))
+        colormap = plt.cm.get_cmap("tab20", len(cluster_types))
+
+        
+        for i, cluster_type in enumerate(cluster_types):
+            index = np.where(anno1 == cluster_type)[0]
+
+            if index.shape[0] != 0:
+                axs[0].scatter(z1[index,0], z1[index,1], color = colormap(i), label = cluster_type, **_kwargs)
+        # axs[0].legend(fontsize = font_size)
+        axs[0].legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = 1, bbox_to_anchor=(1.04, 1))
+        axs[0].set_title("scRNA-Seq", fontsize = 25)
+
+        axs[0].tick_params(axis = "both", which = "major", labelsize = 15)
+
+        axs[0].set_xlabel(axis_label + " 1", fontsize = 19)
+        axs[0].set_ylabel(axis_label + " 2", fontsize = 19)
+        axs[0].set_xlim(np.min(np.concatenate((z1[:,0], z2[:,0]))), np.max(np.concatenate((z1[:,0], z2[:,0]))))
+        axs[0].set_ylim(np.min(np.concatenate((z1[:,1], z2[:,1]))), np.max(np.concatenate((z1[:,1], z2[:,1]))))
+        axs[0].spines['right'].set_visible(False)
+        axs[0].spines['top'].set_visible(False)  
+
+
+        for i, cluster_type in enumerate(cluster_types):
+            index = np.where(anno2 == cluster_type)[0]
+
+            if index.shape[0] != 0:
+                axs[1].scatter(z2[index,0], z2[index,1], color = colormap(i), label = cluster_type, **_kwargs)
+        # axs[1].axis("off")
+        axs[1].legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = 1, bbox_to_anchor=(1.04, 1))
+        axs[1].set_title("scATAC-Seq", fontsize = 25)
+
+        axs[1].tick_params(axis = "both", which = "major", labelsize = 15)
+
+        axs[1].set_xlabel(axis_label + " 1", fontsize = 19)
+        axs[1].set_ylabel(axis_label + " 2", fontsize = 19)
+        axs[1].set_xlim(np.min(np.concatenate((z1[:,0], z2[:,0]))), np.max(np.concatenate((z1[:,0], z2[:,0]))))
+        axs[1].set_ylim(np.min(np.concatenate((z1[:,1], z2[:,1]))), np.max(np.concatenate((z1[:,1], z2[:,1]))))
+        axs[1].spines['right'].set_visible(False)
+        axs[1].spines['top'].set_visible(False)  
+
+    if save:
+        fig.savefig(save, bbox_inches = "tight")
+    
+    print(save)
+
+
