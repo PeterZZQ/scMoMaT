521002e2368fc8ea4edb934d2e7bb98dd2c6daf5
riemannnn@gmail.com <riemannnn@gmail.com>
2021-03-08 22:35:20 -0800

    update cfrm model

--- src/model.py
+++ src/model.py
@@ -3,16 +3,22 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 import torch.optim as opt
+import pandas as pd
 from torch.nn import Module, Parameter
+from torch import softmax, log_softmax
+from torch_sparse import SparseTensor
 from sklearn.cluster import KMeans
-from utils import preprocess
+import scipy.sparse as sp
+from scipy.sparse.linalg import svds
+import time
+from utils import preprocess, csr2st
 
 
 class cfrmModel(Module):
     def __init__(self, dir, N=3, batch_size=100, lr=1e-3, dropout=0.1):
         super().__init__()
         self.batch_size = batch_size
-        self.alpha = torch.FloatTensor([1, 1, 1, 1, 10])
+        self.alpha = torch.FloatTensor([1, 1, 1, 1, 1, 1])
         self.dropout = dropout
         # data
         counts_rna = np.loadtxt(os.path.join(dir, 'GxC1.txt')).T
@@ -41,50 +47,52 @@ class cfrmModel(Module):
         self.C_r = Parameter(v_r[:, :N])
         self.A_2r = self.C_2.t() @ self.R @ self.C_r
         self.D_12 = torch.diag(s_g[:N] / s_r[:N])
-        loss = self.batch_loss('valid')
-        for l in loss:
-            print(l.item())
-        # optimizer
+        self.W = Parameter(torch.eye(N))
+
         self.optimizer = opt.Adam(self.parameters(), lr=lr)
 
     @staticmethod
     def orthogonal_loss(A):
         return (A.t() @ A - torch.eye(A.shape[1])).pow(2).sum()
 
+    def entropy_loss(self, C):
+        z = C @ self.W
+        loss = - F.softmax(z, dim=1) * F.log_softmax(z, dim=1)
+        return loss.sum()
+
     def batch_loss(self, mode='C_c'):
         if mode == 'C_c':
             loss1 = (self.G - self.C_1 @ self.A_1g.detach() @ self.C_g.detach().t()).pow(2).mean()
             loss2 = (self.R - self.C_2 @ self.A_2r.detach() @ self.C_r.detach().t()).pow(2).mean()
             loss5 = sum(map(self.orthogonal_loss, [self.C_1, self.C_2]))
+            loss6 = sum(map(self.entropy_loss, [self.C_1, self.C_2]))
             loss3, loss4 = 0, 0
         elif mode == 'C_r':
             loss2 = (self.R - self.C_2.detach() @ self.A_2r.detach() @ self.C_r.t()).pow(2).mean()
             loss3 = (self.A - self.C_r @ (self.D_gr.detach() * self.C_g.detach().t())).pow(2).mean()
             loss5 = sum(map(self.orthogonal_loss, [self.C_r]))
+            loss6 = 0
             loss1, loss4 = 0, 0
         elif mode == 'C_g':
             loss1 = (self.G - self.C_1.detach() @ self.A_1g.detach() @ self.C_g.t()).pow(2).mean()
             loss3 = (self.A - self.C_r.detach() @ (self.D_gr.detach() * self.C_g.t())).pow(2).mean()
             loss5 = sum(map(self.orthogonal_loss, [self.C_g]))
+            loss6 = 0
             loss2, loss4 = 0, 0
         elif mode == 'valid':
-            with torch.no_grad():
-                loss1 = (self.G - self.C_1 @ self.A_1g @ self.C_g.t()).pow(2).mean()
-                loss2 = (self.R - self.C_2 @ self.A_2r @ self.C_r.t()).pow(2).mean()
-                loss3 = (self.A - self.C_r @ (self.D_gr * self.C_g.t())).pow(2).mean()
-                loss4 = (self.A_1g - self.D_12 * self.A_2r).pow(2).mean()
-                loss5 = sum(map(self.orthogonal_loss, [self.C_1, self.C_2, self.C_g, self.C_r]))
+            loss1 = (self.G - self.C_1 @ self.A_1g @ self.C_g.t()).pow(2).mean().detach()
+            loss2 = (self.R - self.C_2 @ self.A_2r @ self.C_r.t()).pow(2).mean().detach()
+            loss3 = (self.A - self.C_r @ (self.D_gr * self.C_g.t())).pow(2).mean().detach()
+            loss4 = (self.A_1g - self.D_12 * self.A_2r).pow(2).mean().detach()
+            loss5 = sum(map(self.orthogonal_loss, [self.C_1, self.C_2, self.C_g, self.C_r])).detach()
+            loss6 = sum(map(self.entropy_loss, [self.C_1, self.C_2])).detach()
+        else:
+            raise NotImplementedError
         loss = self.alpha[0] * loss1 + self.alpha[1] * loss2 + self.alpha[2] * loss3 + self.alpha[3] * loss4 + \
-               self.alpha[4] * loss5
-        # loss1 = (self.G - self.C_1 @ self.A_1g @ self.C_g.t()).pow(2).sum()
-        # loss2 = (self.R - self.C_2 @ self.A_2r @ self.C_r.t()).pow(2).sum()
-        # loss3 = (self.A - self.C_r @ (self.D_gr * self.C_g.t())).pow(2).sum()
-        # loss4 = (self.A_1g - self.D_12 * self.A_2r).pow(2).mean()
-        # loss5 = sum(map(self.orthogonal_loss, [self.C_1, self.C_2, self.C_g, self.C_r]))
-        # loss = self.alpha[0] * loss1 + self.alpha[1] * loss2 + self.alpha[2] * loss3 + self.alpha[3] * loss4 + self.alpha[4] * loss5
-        # return loss
+               self.alpha[4] * loss5 + self.alpha[5] * loss6
+
         return loss, self.alpha[0] * loss1, self.alpha[1] * loss2, self.alpha[2] * loss3, self.alpha[3] * loss4, \
-               self.alpha[4] * loss5
+               self.alpha[4] * loss5, self.alpha[5] * loss6
 
     def train_func(self, T):
         best_loss = 1e12
@@ -92,14 +100,14 @@ class cfrmModel(Module):
         for t in range(T):
             self.optimizer.zero_grad()
             for mode in ['C_c']:
-                # for mode in ['C_c', 'C_r', 'C_g']:
-                loss, loss1, loss2, loss3, loss4, loss5 = self.batch_loss(mode)
+            # for mode in ['C_c', 'C_r', 'C_g']:
+                loss, *_ = self.batch_loss(mode)
                 loss.backward()
                 self.optimizer.step()
             self.A_1g = self.C_1.t() @ self.G @ self.C_g
             self.A_2r = self.C_2.t() @ self.R @ self.C_r
             self.D_12 = torch.diag((self.A_1g * self.A_2r).sum(dim=1) / self.A_2r.pow(2).sum(dim=1)).detach()
-            loss, loss1, loss2, loss3, loss4, loss5 = self.batch_loss('valid')
+            loss, loss1, loss2, loss3, loss4, loss5, loss6 = self.batch_loss('valid')
             print('Epoch {}, Training Loss: {:.4f}'.format(t + 1, loss.item()))
             info = [
                 'loss RNA: {:.5f}'.format(loss1.item()),
@@ -107,6 +115,7 @@ class cfrmModel(Module):
                 'loss gene act: {:.5f}'.format(loss3.item()),
                 'loss merge: {:.5f}'.format(loss4.item()),
                 'loss ortho: {:.5f}'.format(loss5.item()),
+                'loss entropy: {:.5f}'.format(loss6.item())
             ]
             for i in info:
                 print("\t", i)
@@ -123,9 +132,240 @@ class cfrmModel(Module):
                     else:
                         count = 0
 
+class cfrmSparseModel(Module):
+    def __init__(self, dir, N=3, batch_size=1.0, interval=10, lr=1e-3, init='svd'):
+        super().__init__()
+        self.batch_size = batch_size
+        self.interval = interval
+        self.alpha = torch.FloatTensor([1, 1, 1, 1, 1, 1])
+        # data
+        counts_rna = sp.load_npz(os.path.join(dir, 'C1xG.npz'))
+        counts_atac = sp.load_npz(os.path.join(dir, 'C2xR.npz')).astype(np.float32)
+        Cor = sp.load_npz(os.path.join(dir, 'GxR.npz')).astype(np.float32)
 
+        self.G = torch.FloatTensor(counts_rna.todense())
+        self.R = torch.FloatTensor(counts_atac.todense())
+        self.M = torch.FloatTensor(Cor.todense())
+
+        if init == 'svd':
+            self.A = Parameter(torch.eye(N))
+            u_g, s_g, v_g = torch.svd_lowrank(self.G, N)
+            self.C_1 = Parameter(u_g)
+            self.A_1g_l = Parameter(s_g.sqrt().reshape(-1, 1))
+            self.A_1g_r = Parameter(s_g.sqrt().reshape(1, -1))
+            self.C_g = Parameter(v_g)
+            u_r, s_r, v_r = torch.svd_lowrank(self.R, N)
+            self.C_2 = Parameter(u_r)
+            self.A_2r_l = Parameter(s_r.sqrt().reshape(-1, 1))
+            self.A_2r_r = Parameter(s_r.sqrt().reshape(1, -1))
+            self.C_r = Parameter(v_r)
+            self.A_gr_l = Parameter(torch.ones(N, 1))
+            self.A_gr_r = Parameter(torch.ones(1, N))
+        else:
+            self.C_1 = Parameter(torch.randn(self.G.shape[0], N))
+            self.C_2 = Parameter(torch.randn(self.R.shape[0], N))
+            self.C_g = Parameter(torch.randn(self.G.shape[1], N))
+            self.C_r = Parameter(torch.randn(self.R.shape[1], N))
+            self.A = Parameter(torch.randn(N, N))
+            self.A_1g_l = Parameter(torch.randn(N, 1))
+            self.A_1g_r = Parameter(torch.randn(1, N))
+            self.A_2r_l = Parameter(torch.randn(N, 1))
+            self.A_2r_r = Parameter(torch.randn(1, N))
+            self.A_gr_l = Parameter(torch.randn(N, 1))
+            self.A_gr_r = Parameter(torch.randn(1, N))
+        assert self.M.shape[0] == self.G.shape[1]
+        assert self.M.shape[1] == self.R.shape[1]
+        self.meta_rna = pd.read_csv(os.path.join(dir, "meta_rna.csv"), index_col=0)
+        self.meta_atac = pd.read_csv(os.path.join(dir, "meta_atac.csv"), index_col=0)
+        self.regions = pd.read_csv(os.path.join(dir, "regions.txt"), header=None)
+        self.genes = pd.read_csv(os.path.join(dir, "genes.txt"), header=None)
+
+        self.optimizer = opt.Adam(self.parameters(), lr=lr)
+
+        # with torch.no_grad():
+        #     loss, *_ = self.batch_loss('valid')
+        #     print('Initial Loss is {:.5f}'.format(loss.item()))
+
+
+    def batch_loss(self, mode='C_c'):
+        mask_1 = np.random.choice(self.G.shape[0], int(self.G.shape[0] * self.batch_size), replace=False)
+        mask_2 = np.random.choice(self.R.shape[0], int(self.R.shape[0] * self.batch_size), replace=False)
+        mask_g = np.random.choice(self.G.shape[1], int(self.G.shape[1] * self.batch_size), replace=False)
+        mask_r = np.random.choice(self.R.shape[1], int(self.R.shape[1] * self.batch_size), replace=False)
+        if mode == 'C_12':
+            loss1 = (self.G[np.ix_(mask_1, mask_g)] - self.C_1[mask_1, :] @ (self.A_1g_l * self.A * self.A_1g_r).detach() @
+                     self.C_g[mask_g].detach().t()).pow(2).mean()
+            loss2 = (self.R[np.ix_(mask_2, mask_r)] - self.C_2[mask_2] @ (self.A_2r_l * self.A * self.A_2r_r).detach() @
+                     self.C_r[mask_r].detach().t()).pow(2).mean()
+            loss4 = self.C_1[mask_1].abs().mean() + self.C_2[mask_2].abs().mean()
+            loss3 = 0
+        elif mode == 'C_gr':
+            loss1 = (self.G[np.ix_(mask_1, mask_g)] - self.C_1[mask_1].detach() @ (self.A_1g_l * self.A * self.A_1g_r).detach() @ self.C_g[mask_g].t()).pow(2).mean()
+            loss2 = (self.R[np.ix_(mask_2, mask_r)] - self.C_2[mask_2].detach() @ (self.A_2r_l * self.A * self.A_2r_r).detach() @ self.C_r[mask_r].t()).pow(2).mean()
+            loss3 = (self.M[np.ix_(mask_g, mask_r)] - self.C_g[mask_g] @ (self.A_gr_l * self.A * self.A_gr_r).detach() @ self.C_r[mask_r].t()).pow(2).mean()
+            loss4 = self.C_g[mask_g].abs().mean() + self.C_r[mask_r].abs().mean()
+        elif mode == 'A':
+            loss1 = (self.G[np.ix_(mask_1, mask_g)] - self.C_1[mask_1].detach() @ (self.A_1g_l * self.A * self.A_1g_r) @ self.C_g[mask_g].detach().t()).pow(2).mean()
+            loss2 = (self.R[np.ix_(mask_2, mask_r)] - self.C_2[mask_2].detach() @ (self.A_2r_l * self.A * self.A_2r_r) @ self.C_r[mask_r].detach().t()).pow(2).mean()
+            loss3 = (self.M[np.ix_(mask_g, mask_r)] - self.C_g[mask_g].detach() @ (self.A_gr_l * self.A * self.A_gr_r) @ self.C_r[mask_r].detach().t()).pow(2).mean()
+            loss4 = 0
+        elif mode == 'valid':
+            with torch.no_grad():
+                loss1 = (self.G - self.C_1 @ (self.A_1g_l * self.A * self.A_1g_r) @ self.C_g.t()).pow(2).mean()
+                loss2 = (self.R - self.C_2 @ (self.A_2r_l * self.A * self.A_2r_r) @ self.C_r.t()).pow(2).mean()
+                loss3 = (self.M - self.C_g @ (self.A_gr_l * self.A * self.A_gr_r) @ self.C_r.t()).pow(2).mean()
+                loss4 = (self.C_1.abs().mean() + self.C_2.abs().mean() + self.C_g.abs().mean() + self.C_r.abs().mean())
+        else:
+            raise NotImplementedError
+        loss = self.alpha[0] * loss1 + self.alpha[1] * loss2 + self.alpha[2] * loss3 + self.alpha[3] * loss4
+
+        return loss, self.alpha[0] * loss1, self.alpha[1] * loss2, self.alpha[2] * loss3, self.alpha[3] * loss4
+    #
+    def train_func(self, T):
+        best_loss = 1e12
+        count = 0
+        for t in range(T):
+            self.optimizer.zero_grad()
+            # for mode in ['C_c']:
+            for mode in ['C_12', 'C_gr', 'C_12', 'C_gr', 'A']:
+                loss, *_ = self.batch_loss(mode)
+                loss.backward()
+                self.optimizer.step()
+            if (t+1) % self.interval == 0:
+                loss, loss1, loss2, loss3, loss4 = self.batch_loss('valid')
+                print('Epoch {}, Training Loss: {:.4f}'.format(t + 1, loss.item()))
+                info = [
+                    'loss RNA: {:.5f}'.format(loss1.item()),
+                    'loss ATAC: {:.5f}'.format(loss2.item()),
+                    'loss gene act: {:.5f}'.format(loss3.item()),
+                    'loss sparse: {:.5f}'.format(loss4.item()),
+                ]
+                for i in info:
+                    print("\t", i)
+            if loss.item() < best_loss:
+                best_loss = loss.item()
+                torch.save(self.state_dict(), '../check_points/real.pt')
+                count = 0
+            else:
+                count += 1
+                if count % 20 == 0:
+                    self.optimizer.param_groups[0]['lr'] *= 0.5
+                    print('Epoch: {}, shrink lr to {:.4f}'.format(t + 1, self.optimizer.param_groups[0]['lr']))
+                    if self.optimizer.param_groups[0]['lr'] < 1e-4:
+                        break
+                    else:
+                        self.load_state_dict(torch.load('../check_points/real.pt'))
+                        count = 0
+
+# class cfrmDeepModel(Module):
+#     def __init__(self, dir, N=3, batch_size=100, lr=1e-3, dropout=0.1):
+#         super().__init__()
+#         self.N = N
+#         self.batch_size = batch_size
+#         self.alpha = torch.FloatTensor([1, 1, 1, 1, 1, 1])
+#         self.dropout = dropout
+#         # data
+#         counts_rna = np.loadtxt(os.path.join(dir, 'GxC1.txt')).T
+#         counts_atac = np.loadtxt(os.path.join(dir, 'RxC2.txt')).T
+#
+#         counts_rna = preprocess(counts=counts_rna, mode="quantile", modality="RNA")
+#         counts_atac = preprocess(counts=counts_atac, mode="quantile", modality="ATAC")
+#
+#         self.G = torch.FloatTensor(counts_rna)
+#         self.R = torch.FloatTensor(counts_atac)
+#         self.A = torch.FloatTensor(np.loadtxt(os.path.join(dir, 'RxG.txt')))
+#         assert self.A.shape[0] == self.R.shape[1]
+#         assert self.A.shape[1] == self.G.shape[1]
+#         self.label_g = torch.LongTensor(np.loadtxt(os.path.join(dir, 'gene_label.txt')))
+#         self.label_c1 = torch.LongTensor(np.loadtxt(os.path.join(dir, 'cell_label_C1.txt'), skiprows=1, usecols=[1]))
+#         self.label_c2 = torch.LongTensor(np.loadtxt(os.path.join(dir, 'cell_label_C2.txt'), skiprows=1, usecols=[1]))
+#
+#         self.C_1 = Parameter(torch.rand(self.G.shape[0], N))
+#         self.C_2 = Parameter(torch.rand(self.R.shape[0], N))
+#         self.C_g = Parameter(torch.randn(self.G.shape[1], N))
+#         self.C_r = Parameter(torch.randn(self.R.shape[1], N))
+#         self.A_1g = Parameter(torch.eye(N))
+#         self.A_2r = Parameter(torch.eye(N))
+#         self.A_12 = torch.ones(N, 1)
+#         self.W = Parameter(torch.eye(N))
+#
+#         self.optimizer = opt.Adam(self.parameters(), lr=lr)
+#
+#     def entropy_loss(self, C):
+#         z = softmax(C, dim=1) @ self.W
+#         print(z.shape)
+#         loss = - softmax(z, dim=1) * log_softmax(z, dim=1)
+#         print(loss.sum(dim=1).mean().item())
+#         return loss.sum(dim=1).mean()
+#
+#     def batch_loss(self, mode='C_c'):
+#         if mode == 'C_c':
+#             loss1 = (self.G - softmax(self.C_1, dim=1) @ self.A_1g.detach() @ self.C_g.detach().t()).pow(2).mean()
+#             loss2 = (self.R - softmax(self.C_2, dim=1) @ self.A_2r.detach() @ self.C_r.detach().t()).pow(2).mean()
+#             loss5 = sum(map(self.entropy_loss, [self.C_1, self.C_2]))
+#             loss3, loss4 = 0, 0
+#         elif mode == 'C_r':
+#             loss2 = (self.R - softmax(self.C_2.detach(), dim=1) @ self.A_2r.detach() @ self.C_r.t()).pow(2).mean()
+#             loss3 = (self.A - self.C_r @ self.C_g.detach().t()).pow(2).mean()
+#             loss1, loss4, loss5 = 0, 0, 0
+#         elif mode == 'C_g':
+#             loss1 = (self.G - softmax(self.C_1.detach(), dim=1) @ self.A_1g.detach() @ self.C_g.t()).pow(2).mean()
+#             loss3 = (self.A - self.C_r.detach() @ self.C_g.t()).pow(2).mean()
+#             loss2, loss4, loss5 = 0, 0, 0
+#         elif mode == 'A':
+#             loss1 = (self.G - softmax(self.C_1.detach(), dim=1) @ self.A_1g @ self.C_g.detach().t()).pow(2).mean()
+#             loss2 = (self.R - softmax(self.C_2.detach(), dim=1) @ self.A_2r @ self.C_r.detach().t()).pow(2).mean()
+#             loss3, loss4, loss5 = 0, 0, 0
+#         elif mode == 'valid':
+#             with torch.no_grad():
+#                 loss1 = (self.G - softmax(self.C_1, dim=1) @ self.A_1g @ self.C_g.t()).pow(2).mean()
+#                 loss2 = (self.R - softmax(self.C_2, dim=1) @ self.A_2r @ self.C_r.t()).pow(2).mean()
+#                 loss3 = (self.A - self.C_r @ self.C_g.t()).pow(2).mean()
+#                 loss4 = (self.A_1g - self.D_12 * self.A_2r).pow(2).mean()
+#                 loss5 = sum(map(self.entropy_loss, [self.C_1, self.C_2]))
+#         else:
+#             raise NotImplementedError
+#
+#         loss = self.alpha[0] * loss1 + self.alpha[1] * loss2 + self.alpha[2] * loss3 + self.alpha[3] * loss4 + \
+#                self.alpha[4] * loss5
+#         return loss, loss1, loss2, loss3, loss4, loss5
+#
+#     def train_func(self, T):
+#         best_loss = 1e12
+#         count = 0
+#         for t in range(T):
+#             for mode in ['C_c', 'C_r', 'C_g', 'A']:
+#                 self.optimizer.zero_grad()
+#                 loss, *_ = self.batch_loss(mode)
+#                 loss.backward()
+#                 self.optimizer.step()
+#             self.D_12 = ((self.A_1g * self.A_2r).sum(dim=1) / self.A_2r.pow(2).sum(dim=1)).detach()
+#             loss, loss1, loss2, loss3, loss4, loss5 = self.batch_loss('valid')
+#             print('Epoch {}, Training Loss: {:.4f}'.format(t + 1, loss.item()))
+#             info = [
+#                 'loss RNA: {:.5f}'.format(loss1.item()),
+#                 'loss ATAC: {:.5f}'.format(loss2.item()),
+#                 'loss gene act: {:.5f}'.format(loss3.item()),
+#                 'loss merge: {:.5f}'.format(loss4.item()),
+#                 'loss entropy: {:.5f}'.format(loss5.item())
+#             ]
+#             for i in info:
+#                 print("\t", i)
+#             if loss.item() < best_loss:
+#                 best_loss = loss.item()
+#                 torch.save(self.state_dict(), f'../check_points/simulated_2b_{self.N}c.pt')
+#                 count = 0
+#             else:
+#                 count += 1
+#                 if count % 20 == 0:
+#                     self.load_state_dict(torch.load(f'../check_points/simulated_2b_{self.N}c.pt'))
+#                     self.optimizer.param_groups[0]['lr'] *= 0.5
+#                     print('Epoch: {}, shrink lr to {:.4f}'.format(t + 1, self.optimizer.param_groups[0]['lr']))
+#                     if self.optimizer.param_groups[0]['lr'] < 1e-4:
+#                         break
+#                     else:
+#                         count = 0
 
 if __name__ == '__main__':
-    model = cfrmModel(dir = '../data/simulated/2batches_3clusts', N=3, dropout=0)
+    model = cfrmSparseModel(dir = '../data/real/BMMC/', N=20, lr=1e-3, interval=1, batch_size=0.2)
     model.train_func(T=10000)
-    torch.save(model.state_dict(), '../check_points/simulated_2b_3c.pt')
