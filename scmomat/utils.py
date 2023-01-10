fea2c69a2d128cd4e8eb33620544915584acde14
PeterZZQ <zhangziqibuaa@gmail.com>
2023-01-09 19:07:30 -0500

    fix bug

--- scmomat/utils.py
+++ scmomat/utils.py
@@ -94,6 +94,126 @@ def preprocess(counts, modality = "RNA", log = True):
 
 # ----------------------------------------------------- # 
 
+def plot_latent(zs, annos = None, batches = None, mode = "joint", save = None, figsize = (20,10), axis_label = "Latent", label_inplace = False, **kwargs):
+    """\
+    Description
+        Plot latent space
+    Parameters
+        z1
+            the latent space of first data batch, of the shape (n_samples, n_dimensions)
+        z2
+            the latent space of the second data batch, of the shape (n_samples, n_dimensions)
+        anno1
+            the cluster annotation of the first data batch, of the  shape (n_samples,)
+        anno2
+            the cluster annotation of the second data batch, of the  shape (n_samples,)
+        mode
+            "joint": plot two latent spaces(from two batches) into one figure
+            "separate" plot two latent spaces separately
+        save
+            file name for the figure
+        figsize
+            figure size
+    """
+    _kwargs = {
+        "s": 10,
+        "alpha": 0.9,
+        "markerscale": 1,
+        "text_size": "large",
+        "colormap": "tab20b"
+    }
+    _kwargs.update(kwargs)
+
+    fig = plt.figure(figsize = figsize)
+    if mode == "joint":
+        try:
+            assert batches is None
+        except:
+            raise ValueError("batches should only be assigned when mode is ``separate''. ")
+        try:
+            assert annos is not None
+        except:
+            raise ValueError("annos should be provided when mode is ``joint''. ")
+
+
+        ax = fig.add_subplot()
+        unique_cluster = np.unique(annos)
+        colormap = plt.cm.get_cmap(_kwargs["colormap"], len(unique_cluster))
+        unique_cluster = sorted(list(unique_cluster))
+
+        texts = []
+        for i, cluster_type in enumerate(unique_cluster):
+            index = np.where(annos == cluster_type)[0]
+            z_clust = zs[index,:]
+            ax.scatter(z_clust[:,0], z_clust[:,1], color = colormap(i), label = cluster_type, s = _kwargs["s"], alpha = _kwargs["alpha"])
+            # text on plot
+            if label_inplace:
+                texts.append(ax.text(np.median(z_clust[:,0]), np.median(z_clust[:,1]), color = "black", s = unique_cluster[i], fontsize = _kwargs["text_size"], weight = 'semibold', in_layout = True))
+        
+        ax.legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = (len(unique_cluster) // 15) + 1, bbox_to_anchor=(1.04, 1), markerscale = _kwargs["markerscale"])
+        
+        ax.tick_params(axis = "both", which = "major", labelsize = 15)
+
+        ax.set_xlabel(axis_label + " 1", fontsize = 19)
+        ax.set_ylabel(axis_label + " 2", fontsize = 19)
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)  
+        # adjust position
+        if label_inplace:
+            if adjust_text_flag:
+                adjust_text(texts, only_move={'points':'xy', 'texts':'xy'})
+
+    elif mode == "separate":
+        try:
+            assert batches is not None
+            assert annos is not None
+        except:
+            raise ValueError("batches and annos should all be provided when mode is ``separate''.")
+
+        unique_batch = np.unique(batches)
+        unique_cluster = np.unique(annos) 
+        axs = fig.subplots(len(unique_batch),1)
+        colormap = plt.cm.get_cmap(_kwargs["colormap"], len(unique_cluster))
+
+
+        for i, batch in enumerate(unique_batch):
+            zs_batch = zs[batches == batch]
+            annos_batch = annos[batches == batch]
+            z_clust = []
+            texts = []
+            for j, cluster_type in enumerate(unique_cluster):
+                index = np.where(annos_batch == cluster_type)[0]
+                if len(index) > 0:
+                    axs[i].scatter(zs_batch[index,0], zs_batch[index,1], color = colormap(j), label = cluster_type, s = _kwargs["s"], alpha = _kwargs["alpha"])
+                    # text on plot
+                    if label_inplace:
+                        # if exist cells
+                        if zs_batch[index,0].shape[0] > 0:
+                            texts.append(axs[i].text(np.median(zs_batch[index,0]), np.median(zs_batch[index,1]), color = "black", s = cluster_type, fontsize = _kwargs["text_size"], weight = 'semibold', in_layout = True))
+            
+            axs[i].legend(loc='upper left', prop={'size': 15}, frameon = False, ncol = (len(unique_cluster) // 15) + 1, bbox_to_anchor=(1.04, 1), markerscale = _kwargs["markerscale"])
+            axs[i].set_title(batch, fontsize = 25)
+
+            axs[i].tick_params(axis = "both", which = "major", labelsize = 15)
+
+            axs[i].set_xlabel(axis_label + " 1", fontsize = 19)
+            axs[i].set_ylabel(axis_label + " 2", fontsize = 19)
+
+            axs[i].spines['right'].set_visible(False)
+            axs[i].spines['top'].set_visible(False)  
+
+            axs[i].set_xlim(np.min(zs[:,0]), np.max(zs[:,1]))
+            axs[i].set_ylim(np.min(zs[:,0]), np.max(zs[:,1]))
+
+            if label_inplace:
+                if adjust_text_flag:
+                    adjust_text(texts, only_move={'points':'xy', 'texts':'xy'})        
+
+    plt.tight_layout()
+    if save:
+        fig.savefig(save, bbox_inches = "tight")
+
+
 def plot_latent_ext(zs, annos = None, mode = "joint", save = None, figsize = (20,10), axis_label = "Latent", label_inplace = False, **kwargs):
     """\
     Description
